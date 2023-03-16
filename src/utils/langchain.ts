04cd630a221cb0cb0cc7e36ac2f853e102504205
typeleven <typeleven@gmail.com>
2023-03-16 06:45:14 -0600

    changing to logging

--- src/utils/langchain.ts
+++ src/utils/langchain.ts
@@ -8,31 +8,32 @@ import {
 } from "langchain/callbacks";
 import { LLMResult } from "langchain/dist/schema";
 
-const callbackManager = CallbackManager.fromHandlers({
-  async handleLLMEnd(output: LLMResult) {
-    console.log("handleLLMEnd", output);
-    logger.verbose({
-      _source: ["callbackManager", "fromHandlers", "handleLLMEnd", "response"],
-      payload: output,
-      usage: output.llmOutput?.tokenUsage,
-    });
-  },
-});
+// disabling for now until support for token usage is added
+// const callbackManager = CallbackManager.fromHandlers({
+//   async handleLLMEnd(output: LLMResult) {
+//     console.log("handleLLMEnd", output);
+//     logger.verbose({
+//       _source: ["callbackManager", "fromHandlers", "handleLLMEnd", "response"],
+//       payload: output,
+//       usage: output.llmOutput?.tokenUsage,
+//     });
+//   },
+// });
 
-// disabled until we can figure out how to get the token usage working
+const callbackManager = new CallbackManager();
 
-// callbackManager.handleLLMStart = async (..._args) => {
-//   logger.verbose({
-//     _source: ["callbackManager", "handleLLMStart", "request"],
-//     payload: _args,
-//   });
-// };
-// callbackManager.handleLLMEnd = async (..._args) => {
-//   logger.verbose({
-//     _source: ["callbackManager", "handleLLMEnd", "response"],
-//     payload: _args,
-//   });
-// };
+callbackManager.handleLLMStart = async (..._args) => {
+  logger.verbose({
+    _source: ["callbackManager", "handleLLMStart", "request"],
+    payload: _args,
+  });
+};
+callbackManager.handleLLMEnd = async (..._args) => {
+  logger.verbose({
+    _source: ["callbackManager", "handleLLMEnd", "response"],
+    payload: _args,
+  });
+};
 
 const makeChain = (
   vectorstore: SupabaseVectorStore,
