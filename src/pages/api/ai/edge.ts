3513350b88c3b83a2a7af1d2be1c6686d1f4e3cd
typeleven <typeleven@gmail.com>
2023-03-23 11:05:09 -0600

    edge test

---
+++ src/pages/api/ai/edge.ts
@@ -0,0 +1,31 @@
+import { NextRequest } from "next/server";
+import { OpenAIStream } from "@/utils/open_ai_stream";
+
+export const config = {
+  runtime: "edge",
+};
+
+const handler = async (req: Request): Promise<Response> => {
+  //   const { prompt } = (await req.json()) as {
+  //     prompt?: string;
+  //   };
+
+  const prompt = "tell me a 100 word story about a cat";
+
+  const payload = {
+    model: "text-davinci-003",
+    prompt,
+    temperature: 0.7,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    max_tokens: 200,
+    stream: true,
+    n: 1,
+  };
+
+  const stream = await OpenAIStream(payload);
+  return new Response(stream);
+};
+
+export default handler;
